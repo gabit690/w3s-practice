129bd6b1c8687b7151c04d0da21cbd79412b3c00
gabit <gabit690@gmail.com>
2020-07-29 17:06:30 -0300

    uso de eventos de la API

--- projects/google-maps/google-maps-scripts.js
+++ projects/google-maps/google-maps-scripts.js
@@ -1,34 +1,31 @@
   
-function iniciarMapa(){
+function iniciarMapa() {
 
   let fiuba = new google.maps.LatLng(-34.617529, -58.368317);
   let casaRosada = new google.maps.LatLng(-34.6080556, -58.3702778);
   let obelisco = new google.maps.LatLng(-34.6037389, -58.3815704);
   let lunaPark = new google.maps.LatLng(-34.6021614, -58.3684158);
   
+  // Creacion del mapa
+
   let propiedadesMapa = {
     center: fiuba,
-    zoom: 14
+    zoom: 14,
+    disableDefaultUI: true,
+    zoomControl: true,
+    mapTypeId: google.maps.MapTypeId.HYBRID
   };
   
   let mapa = new google.maps.Map(document.getElementById('mapa'), propiedadesMapa);
-  
+
   // Marca de Mapa
+
   let marcaCentroDeMapa = new google.maps.Marker({
     position: fiuba,
+    map: mapa,
     animation: google.maps.Animation.BOUNCE,
   });
   
-  marcaCentroDeMapa.setMap(mapa);
-
-  // Información en marca
-
-  let info = new google.maps.InfoWindow({
-    content: "FIUBA"
-  });
-
-  info.open(mapa, marcaCentroDeMapa);
-
   // Polilínea
 
   let destinoPolilinea = [fiuba, obelisco, lunaPark];
@@ -38,10 +35,9 @@ function iniciarMapa(){
     strokeColor: "#0000FF",
     strokeOpacity: 0.5,
     strokeWeight: 3,
-    editable: false
+    editable: false,
+    map: mapa
   });
-
-  camino1.setMap(mapa);
 
   // Polígono
 
@@ -54,24 +50,67 @@ function iniciarMapa(){
     strokeWeight: 3,
     fillColor:"#00FFFF",
     fillOpacity:0.4,
-    editable: false
+    editable: false,
+    map: mapa
   });
-
-  camino2.setMap(mapa);
 
   // Circulo
 
   let rango = new google.maps.Circle({
-    center: fiuba,
+    center: obelisco,
     radius: 500,
-    strokeColor: "#FF00FF",
+    strokeColor: "#F0F0F0",
     strokeOpacity: 0.8,
     strokeWeight: 2,
-    fillColor:" #FF00FF",
+    fillColor:" #F0F0F0",
     fillOpacity: 0.4,
-    editable: false
+    editable: false,
+    map: mapa
   });
 
-  rango.setMap(mapa);
+  // Agregar evento para zoom a la marca del centro
+
+  google.maps.event.addListener(marcaCentroDeMapa, 'click', () => {
+    
+    let zoomInicial = mapa.getZoom();
+    
+    mapa.setZoom(18);
+    
+    let nuevaInfo = new google.maps.InfoWindow({
+      content: "FIUBA"
+    });
+
+    nuevaInfo.open(mapa, marcaCentroDeMapa);
+
+    window.setTimeout(() => {
+      mapa.setZoom(zoomInicial);
+    }, 3000);
+
+  });
+
+  // Funcionalidad para poner marcas con sus coordenadas en el mapa.
+
+  function marcarMapa(mapa, posicion) {
+
+    let nuevaMarca = new google.maps.Marker({
+      position: posicion,
+      map: mapa
+    });
+
+    let infoPosicion = new google.maps.InfoWindow({
+      content: "Latitud: " + posicion.lat() + "<br>Longitud: " + posicion.lng()
+    });
+
+    infoPosicion.open(mapa, nuevaMarca);
+
+  }
+
+  google.maps.event.addListener(mapa, 'click', (event) => {
+
+    console.log(event.latLng);
+    
+    marcarMapa(mapa, event.latLng);
+
+  });
 
 }
