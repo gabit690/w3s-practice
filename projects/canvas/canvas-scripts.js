045fc888167f535a5c6b9fd711e265c336061ed4
gabit <gabit690@gmail.com>
2020-08-03 18:37:45 -0300

    practica de elementos básicos con CANVAS

---
+++ projects/canvas/canvas-scripts.js
@@ -0,0 +1,62 @@
+const lienzo1 = document.getElementById('lienzo1');
+
+const lienzo1Contexto = lienzo1.getContext('2d');
+
+//  RECTANGULO
+// lienzo1Contexto.fillStyle = 'red';
+
+// lienzo1Contexto.fillRect(150, 0, 150, 100);
+
+// LINEA
+// lienzo1Contexto.moveTo(0, 0);
+
+// lienzo1Contexto.lineTo(300, 100);
+
+// lienzo1Contexto.stroke();
+
+// CIRCULO
+
+// lienzo1Contexto.beginPath();
+
+// lienzo1Contexto.arc(150, 100, 50, 0, 2 * Math.PI);
+
+// lienzo1Contexto.stroke();
+
+// GRADIENTES
+
+// const gradienteLineal = lienzo1Contexto.createLinearGradient(0, 0, 200, 0);
+
+// gradienteLineal.addColorStop(0, 'red');
+
+// gradienteLineal.addColorStop(1, 'blue');
+
+// lienzo1Contexto.fillStyle = gradienteLineal;
+
+
+// const gradienteRadial = lienzo1Contexto.createRadialGradient(75, 50, 5, 90, 60, 100);
+
+// lienzo1Contexto.fillStyle = gradienteRadial;
+
+// gradienteRadial.addColorStop(0, 'red');
+
+// gradienteRadial.addColorStop(1, 'blue');
+
+// lienzo1Contexto.fillRect(20, 20, 150, 80);
+
+// TEXTO
+
+// lienzo1Contexto.font = "30px Arial";
+
+// lienzo1Contexto.fillStyle = 'blue';
+
+// lienzo1Contexto.textAling = "center";
+
+// lienzo1Contexto.fillText("Hello World", 50, 100);
+
+//lienzo1Contexto.strokeText("Hello World", 50, 100);
+
+// IMAGE
+
+// const img = document.getElementById("imagen");
+
+// lienzo1Contexto.drawImage(img, 10, 10);
